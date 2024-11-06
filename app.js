7db008c596c586b033d8832b0549b98cdd2bcb83
unknown <kqyz12@gmail.com>
2024-11-06 16:50:13 +0300

    okkey

--- app.js
+++ app.js
@@ -80,20 +80,50 @@
 
 //console.log('Exercise 9 result:', allFoods)
 
-const foods = ['pizza', 'cheeseburger']
+//const foods = ['pizza', 'cheeseburger']
 
-foods.unshift('taco')
+//foods.unshift('taco')
 
-foods.splice(2, 0, 'tofu')
+//foods.splice(2, 0, 'tofu')
 
-foods.splice(1, 1, 'sushi', 'cupcake')
+//foods.splice(1, 1, 'sushi', 'cupcake')
 
-const yummy = foods.slice(1, 3)
+//const yummy = foods.slice(1, 3)
 
-const soyIdx = foods.indexOf('tofu')
+//const soyIdx = foods.indexOf('tofu')
 
-const allFoods = foods.join(' -> ')
+//const allFoods = foods.join(' -> ')
 
-const hasSoup = foods.includes('soup')
+//const hasSoup = foods.includes('soup')
 
-console.log('Exercise 10 result:', hasSoup)
+//console.log('Exercise 10 result:', hasSoup)
+
+//const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90]
+//const odds = []
+//for (let i = 0; i < nums.length; i++) {
+//if (nums[i] % 2 !== 0) {
+//odds.push(nums[i])
+//}
+//}
+
+//console.log('Exercise 11 result:', odds)
+
+const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90]
+
+const fizz = []
+const buzz = []
+const fizzbuzz = []
+for (let i = 0; i <= nums.length; i++) {
+  if (nums[i] % 3 === 0) {
+    fizz.push(nums[i])
+  }
+  if (nums[i] % 5 === 0) {
+    buzz.push(nums[i])
+  }
+  if (nums[i] % 3 === 0 && nums[i] % 5 === 0) {
+    fizzbuzz.push(nums[i])
+  }
+}
+console.log('  fizz:', fizz)
+console.log('  buzz:', buzz)
+console.log('  fizzbuzz:', fizzbuzz)
