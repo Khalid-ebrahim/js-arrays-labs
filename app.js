5017515c9b3dbe10b8f8a762d6b0bd979d021cde
unknown <kqyz12@gmail.com>
2024-11-06 12:18:00 +0300

    all good

---
+++ app.js
@@ -0,0 +1,29 @@
+//const foods = []
+
+//console.log('Exercise 1 result:', foods)
+
+//const foods = ['pizza', 'cheeseburger']
+
+//console.log('Exercise 2 result:', foods)
+
+//foods.unshift('taco')
+
+//console.log('Exercise 3 result:', foods)
+
+//const foods = ['pizza', 'cheeseburger']
+
+//foods.unshift('taco')
+
+//console.log(foods[0])
+
+//const favFood = foods[1]
+
+//console.log('Exercise 4 result:', favFood)
+
+const foods = ['pizza', 'cheeseburger']
+
+foods.unshift('taco')
+
+foods.splice(2, 0, 'tofu')
+
+console.log('Exercise 5 result:', foods)
